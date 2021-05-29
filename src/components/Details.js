4f47c3309778ad21d9d64f0dfe0ea811735d5435
aa-nadim <nadim.ice.nstu@gmail.com>
2021-05-29 14:22:08 +0600

    ShopKori home page with 'https://jsonplaceholder.typicode.com/photos' json file

---
+++ src/components/Details.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Details = () => {
+    return (
+        <div className="container mt-100">
+           <h1>Details</h1>
+        </div>
+    )
+}
+
+export default Details
---
+++ src/store/actions/productActions.js
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+export const loadProducts = () => {
+    return (dispatch) => {
+        fetch(`https://jsonplaceholder.typicode.com/photos`)
+        .then(res=> res.json())
+        .then(data=> {
+            dispatch({
+                type: 'LOAD_PRODUCTS',
+                payload: data
+            })
+        })
+    }
+
+}
