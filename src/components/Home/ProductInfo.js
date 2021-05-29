47ea9c0c392c90f777260895180fd5db5f371766
nrd141913 <nrd141913@.com>
2021-05-29 18:36:29 +0600

    home page added

---
+++ src/components/Home/ProductInfo.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import currencyFormatter from "currency-formatter";
+
+const ProductInfo = (props) => {
+    const { name, image, id, price, discount, discountPrice, description } = props.product;
+    return (
+        <div className="col-lg-4 mt-4">
+            <Card className="shadow">
+                <Link to={`/details/${id}`}><Card.Img variant="top" src={`/images/${image}`} style={{height:'450px',padding:'10px', borderRadius:'15px', display:'block'}} className="animation" /></Link>
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text className="text-muted">{description}</Card.Text>
+                </Card.Body>
+                <Card.Footer className="d-flex justify-content-around">
+                    <div>
+                        <span>{currencyFormatter.format(price, { code: 'USD' })}</span> <span className="text-muted">{discount}%</span>
+                    </div>
+                    <div>
+                        {currencyFormatter.format(discountPrice, { code: 'USD' })}
+                    </div>
+                </Card.Footer>
+            </Card>
+        </div>
+    );
+};
+
+export default ProductInfo;
