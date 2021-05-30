8dc958eb459563c497fec5858ea7fa9fe663aefe
Mizanur Rahman <mizzanur007@gmail.com>
2021-05-30 14:07:35 +0600

    ✔ modified files and recommit of the repo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import './App.css';
-<<<<<<< HEAD
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-=======
-import React from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom"
->>>>>>> 2a193fb430747b4ef410f46563ecba5ff732ea0f
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Home from './components/Home/Home';
 import Cart from './components/Cart/Cart';
@@ -19,37 +14,12 @@ function App() {
     <Router>
       <Provider store={store}>
         <Nav />
-<<<<<<< HEAD
-<<<<<<< HEAD
 
         <Route path="/" exact component={Home} />
         <Route path="/cart" exact component={Cart} />
         <Route path="/details/:id" exact component={Details} />
-=======
-      
-        <Route path="/" exact component={Home} />
-        <Route path="/cart" exact component={Cart} />
-        <Route path="/details/:id" exact component={Details} />
-      
->>>>>>> 2a193fb430747b4ef410f46563ecba5ff732ea0f
-=======
-        <Switch>
 
-          <Route path="/">
-            <Home />
-          </Route>
-
-          <Route path="/cart">
-            <Cart />
-          </Route>
-
-          <Route path="/details/:id">
-            <Details />
-          </Route>
-
-        </Switch>
         <Footer />
->>>>>>> 414f27b081d9c3ed37e877300cdb8ad2735d6c2c
       </Provider>
     </Router>
   );
--- src/components/Home/ProductInfo.js
+++ src/components/Home/ProductInfo.js
@@ -1,34 +1,47 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import currencyFormatter from "currency-formatter";
+import currencyFormatter from 'currency-formatter';
 
 const ProductInfo = (props) => {
-    const { name, image, id, price, discount, discountPrice, description } = props.product;
-    return (
-        <div className="col-lg-6 col-xl-4 mt-4">
-            <Card className="shadow">
-                <Link to={`/details/${id}`}><Card.Img variant="top" src={`/images/${image}`} style={{height:'450px',padding:'10px', borderRadius:'15px', display:'block'}} className="animation" /></Link>
-                <Card.Body>
-                    <Card.Title>{name}</Card.Title>
-                    <Card.Text className="text-muted">{description}</Card.Text>
-                </Card.Body>
-                <Card.Footer className="d-flex justify-content-around">
-<<<<<<< HEAD
-                    <div className="product__price">
-                        <span className="actualPrice">{currencyFormatter.format(price, { code: 'USD' })}</span> <span className="text-muted">{discount}%</span>
-=======
-                    <div>
-                        <span>{currencyFormatter.format(price, { code: 'USD' })}</span> <span className="text-muted"  style={{fontSize:'13px'}}>{discount}%</span>
->>>>>>> 414f27b081d9c3ed37e877300cdb8ad2735d6c2c
-                    </div>
-                    <div className="product__discount__price">
-                        <span className="discount">{currencyFormatter.format(discountPrice, { code: 'USD' })}</span>
-                    </div>
-                </Card.Footer>
-            </Card>
-        </div>
-    );
+  const { name, image, id, price, discount, discountPrice, description } =
+    props.product;
+  return (
+    <div className="col-lg-6 col-xl-4 mt-4">
+      <Card className="shadow">
+        <Link to={`/details/${id}`}>
+          <Card.Img
+            variant="top"
+            src={`/images/${image}`}
+            style={{
+              height: '450px',
+              padding: '10px',
+              borderRadius: '15px',
+              display: 'block',
+            }}
+            className="animation"
+          />
+        </Link>
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text className="text-muted">{description}</Card.Text>
+        </Card.Body>
+        <Card.Footer className="d-flex justify-content-around">
+          <div className="product__price">
+            <span className="actualPrice">
+              {currencyFormatter.format(price, { code: 'USD' })}
+            </span>{' '}
+            <span className="text-muted">{discount}%</span>
+          </div>
+          <div className="product__discount__price">
+            <span className="discount">
+              {currencyFormatter.format(discountPrice, { code: 'USD' })}
+            </span>
+          </div>
+        </Card.Footer>
+      </Card>
+    </div>
+  );
 };
 
 export default ProductInfo;
