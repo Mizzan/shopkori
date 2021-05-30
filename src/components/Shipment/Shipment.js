2c33fac12431f7cf19ff27aa7a65c09895f122af
Mizanur Rahman <mizzanur007@gmail.com>
2021-05-30 15:08:45 +0600

    ✔ modified shipment css and styles

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -10,41 +10,45 @@ const Shipment = () => {
   };
 
   return (
-    <div className="row">
-      <div style={{ display: 'block' }} className="col-md-6">
-        <form className="ship-form" onSubmit={handleSubmit(onSubmit)}>
-          <input
-            name="name"
-            ref={register({ required: true })}
-            placeholder="Your Name"
-          />
-          {errors.name && <span className="error">Name is required</span>}
+    <div className="shipment-block">
+      <div className="row">
+        <div style={{ display: 'block' }} className="col-md-6">
+          <form className="ship-form" onSubmit={handleSubmit(onSubmit)}>
+            <input
+              name="name"
+              ref={register({ required: true })}
+              placeholder="Your Name"
+            />
+            {errors.name && <span className="error">Name is required</span>}
 
-          <input
-            name="email"
-            ref={register({ required: true })}
-            placeholder="Your Email"
-          />
-          {errors.email && <span className="error">Email is required</span>}
+            <input
+              name="email"
+              ref={register({ required: true })}
+              placeholder="Your Email"
+            />
+            {errors.email && <span className="error">Email is required</span>}
 
-          <input
-            name="address"
-            ref={register({ required: true })}
-            placeholder="Your Address"
-          />
-          {errors.address && <span className="error">Address is required</span>}
+            <input
+              name="address"
+              ref={register({ required: true })}
+              placeholder="Your Address"
+            />
+            {errors.address && (
+              <span className="error">Address is required</span>
+            )}
 
-          <input
-            name="phone"
-            ref={register({ required: true })}
-            placeholder="Your Phone Number"
-          />
-          {errors.phone && (
-            <span className="error">Phone Number is required</span>
-          )}
+            <input
+              name="phone"
+              ref={register({ required: true })}
+              placeholder="Your Phone Number"
+            />
+            {errors.phone && (
+              <span className="error">Phone Number is required</span>
+            )}
 
-          <input type="submit" />
-        </form>
+            <input type="submit" />
+          </form>
+        </div>
       </div>
     </div>
   );
