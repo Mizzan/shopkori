5c7f90dd0c25541e27944b51ce48c57b416ffd7f
Mizanur Rahman <mizzanur007@gmail.com>
2021-05-30 15:27:53 +0600

    ✔ added more items and slider items and modified slider

--- src/components/SwiperSlider/SwiperSlider.js
+++ src/components/SwiperSlider/SwiperSlider.js
@@ -5,6 +5,8 @@ import ImageOne from '../images/1.jpg';
 import ImageTwo from '../images/2.jpg';
 import ImageThree from '../images/3.jpg';
 import ImageFour from '../images/4.jpg';
+import ImageFive from '../images/11.jpg';
+import ImageSix from '../images/12.jpg';
 
 // Import Swiper styles
 import 'swiper/swiper.min.css';
@@ -14,15 +16,20 @@ import 'swiper/components/navigation/navigation.min.css';
 import '../../App.css';
 
 // import Swiper core and required modules
-import SwiperCore, { Pagination, Navigation } from 'swiper/core';
+import SwiperCore, { Pagination, Navigation, Autoplay } from 'swiper/core';
 
 // install Swiper modules
-SwiperCore.use([Pagination, Navigation]);
+SwiperCore.use([Pagination, Navigation, Autoplay]);
 
 export default function App() {
   return (
     <>
       <Swiper
+        loop={true}
+        autoplay={{
+          delay: 1000,
+          disableOnInteraction: false,
+        }}
         pagination={{
           type: 'progressbar',
         }}
@@ -40,6 +47,12 @@ export default function App() {
         </SwiperSlide>
         <SwiperSlide>
           <img style={{ width: '300px' }} src={ImageFour} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img style={{ width: '300px' }} src={ImageFive} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img style={{ width: '300px' }} src={ImageSix} alt="" />
         </SwiperSlide>
       </Swiper>
     </>
