a1931054204c03909b8b24ab1f3abce36876aef4
Mizanur Rahman <mizzanur007@gmail.com>
2021-05-30 13:24:40 +0600

    ✔ modified files and updated files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Home from './components/Home/Home';
 import Cart from './components/Cart/Cart';
@@ -13,18 +13,10 @@ function App() {
     <Router>
       <Provider store={store}>
         <Nav />
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
 
-          <Route path="/cart">
-            <Cart />
-          </Route>
-          <Route path="/details/:id">
-            <Details />
-          </Route>
-        </Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/cart" exact component={Cart} />
+        <Route path="/details/:id" exact component={Details} />
       </Provider>
     </Router>
   );
